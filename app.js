cbc76edd89c16fc5b141f8b60f7feaeb1fc00f8f
Miguel-Hrc <136330905+Miguel-Hrc@users.noreply.github.com>
2023-08-01 01:45:48 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let btn = document.querySelector('button');
+5let btn = document.querySelector('button');
 btn.addEventListener('click', function (e) {
     var Input_1 = document.getElementById('first-name');
     var Input_2 = document.getElementById('last-name');
@@ -16,13 +16,13 @@ btn.addEventListener('click', function (e) {
          Error.style.display = "";
     }
 
-    if (Input_3.value =="" ) {
+    if (Input_3.value == "" ) {
         let Error = document.getElementById('error-message');
         e.preventDefault();
          Error.style.display = "";
     }
 
-    else if (Input_1.value && Input_2.value && Input_3.value != "") {
+    else if (Input_1.value && Input_2.value && Input_3.value != "" ) {
         let Error = document.getElementById('error-message');
         e.preventDefault();
         Error.style.display = "none"; 
@@ -54,8 +54,11 @@ btn.addEventListener('click', function (e) {
     div2Clone.appendChild(div3Clone);
     div3Clone.appendChild(commentaireTexte);
 
-    div1Clone.style.display = "";
-
-    
+    div1Clone.style.display = "";  
 }
+});
+btn.addEventListener('click',function resetFields(){
+  document.getElementById('first-name').value="";
+  document.getElementById('last-name').value="";
+  document.getElementById('message').value="";
 });
