3b6da4f01e5f1701a593bd2490daf1fdbabacba5
Miguel-Hrc <136330905+Miguel-Hrc@users.noreply.github.com>
2023-08-01 01:20:34 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 let btn = document.querySelector('button');
 btn.addEventListener('click', function (e) {
     var Input_1 = document.getElementById('first-name');
+    var Input_2 = document.getElementById('last-name');
+    var Input_3 = document.getElementById('message');
 
     if (Input_1.value == "" ) {
         let Error = document.getElementById('error-message');
         e.preventDefault();
          Error.style.display = "";
     }
+    if (Input_2.value == ""){
+        let Error = document.getElementById('error-message');
+        e.preventDefault();
+         Error.style.display = "";
+    }
+
+    if (Input_3.value ==""){
+        let Error = document.getElementById('error-message');
+        e.preventDefault();
+         Error.style.display = "";
+    }
+
   });
